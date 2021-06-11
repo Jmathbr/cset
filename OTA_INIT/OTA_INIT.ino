36aedf22b4bdc3f5ebb2a4998f8e2f469ecffc61
Joao Matheus Bernardo Resende <joao.matheus92225@gmail.com>
2021-06-11 14:34:08 -0300

    sinc server

--- OTA_INIT/OTA_INIT.ino
+++ OTA_INIT/OTA_INIT.ino
@@ -4,8 +4,8 @@
 #include <HTTPClient.h>
 #include "ArduinoJson.h"
 
-const char*   SSID      = "Rede Desconectada";
-const char*   PASSWORD  = "988641988coelho";
+const char*   SSID      = "brisa-1386277";
+const char*   PASSWORD  = "ledafyje";
 const char*   VCS_URL   = "https://cest.imd.ufrn.br/cest-api/firmware/last-update";
 const char*   auth_url = "https://cest.imd.ufrn.br/cest-api/auth";
 String leitura, httpRequestData;
@@ -102,16 +102,17 @@ void setup() {
   }
 
  token = op["jwt"];
+ String mytoken = "Bearer " + String(token);
  Serial.println("----------------------------------------");
  Serial.print("External: Token: ");
  Serial.println(token);
  Serial.println("----------------------------------------");
- 
 //==================================================
 //==================================================
 
-//HTTP GET + JSON
+//HTTP GET  + JSON
   http.begin(VCS_URL);
+  http.addHeader("Authorization",mytoken);
   int httpCode = http.GET();
   String payload = http.getString();
   
@@ -143,6 +144,7 @@ if(v_cloud_value > v_local_value){   // Versao da nuvem superior ao firmware
   Serial.println("Atualizando: ...");
   
   };
+
 
  
 //String md5 = ESP.getSketchMD5();
